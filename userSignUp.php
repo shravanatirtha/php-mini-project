938ee8a421f9479700f1ece7accfff1aa5009d93
shravanatirtha <shravz.ceo@gmail.com>
2021-12-21 21:50:24 +0530

    cleaned

--- userSignUp.php
+++ userSignUp.php
@@ -4,6 +4,7 @@ require_once "config.php";
 
 
 // Define variables and initialize with empty values
+$fullname="";
 $username = $password = $confirm_password = "";
 $username_err = $password_err = $confirm_password_err = "";
 
@@ -20,7 +21,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   }
   else {
     // Prepare a select statement
-    $sql = "SELECT id FROM users WHERE username = ?";
+    $sql = "SELECT UserName FROM users WHERE UserName = ?";
 
     if ($stmt = mysqli_prepare($link, $sql)) {
       // Bind variables to the prepared statement as parameters
@@ -76,13 +77,14 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   if (empty($username_err) && empty($password_err) && empty($confirm_password_err)) {
 
     // Prepare an insert statement
-    $sql = "INSERT INTO users (username, password) VALUES (?, ?)";
-
+    $sql = "INSERT INTO users (FullName, UserName, PassWord) VALUES (?, ?, ?)";
+     
     if ($stmt = mysqli_prepare($link, $sql)) {
       // Bind variables to the prepared statement as parameters
-      mysqli_stmt_bind_param($stmt, "ss", $param_username, $param_password);
+      mysqli_stmt_bind_param($stmt,$param_fullname, $param_username, $param_password);
 
       // Set parameters
+      $param_fullname=$fullname;
       $param_username = $username;
       $param_password = password_hash($password, PASSWORD_DEFAULT); // Creates a password hash
 
@@ -105,107 +107,95 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 }
 ?>
  
-<!DOCTYPE html>
-<!-- saved from url=(0057)http://localhost/college/php-mini-project/user/login.html -->
+ <!DOCTYPE html>
+<!-- saved from url=(0088)https://www.bootstrapdash.com/demo/stellar-admin-free/jquery/pages/samples/register.html -->
 <html lang="en"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-  
-  <!-- Required meta tags -->
-
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <title>Online Book Store</title>
-  <!-- plugins:css -->
-  <link rel="stylesheet" href="./newUser_files/simple-line-icons.css">
-  <link rel="stylesheet" href="./newUser_files/flag-icon.min.css">
-  <link rel="stylesheet" href="./newUser_files/vendor.bundle.base.css">
-
-  <link rel="stylesheet" href="./newUser_files/style.css" <!--="" end="" layout="" styles="" --="">
-  <link rel="shortcut icon" href="https://www.bootstrapdash.com/demo/stellar-admin-free/jquery/images/favicon.png">
-</head>
-
-<body data-new-gr-c-s-check-loaded="14.1042.0" data-gr-ext-installed="">
-  <div class="container-scroller">
-    <div class="container-fluid page-body-wrapper full-page-wrapper">
-      <div class="content-wrapper d-flex align-items-center auth">
-        <div class="row flex-grow">
-          <div class="col-lg-4 mx-auto">
-            <div class="auth-form-light text-left p-5">
-              <div class="brand-logo">
-                <img src="./newUser_files/logo.svg">
+    <!-- Required meta tags -->
+    
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Online Book Store</title>
+    <!-- plugins:css -->
+    <link rel="stylesheet" href="./userSignUp_files/simple-line-icons.css">
+    <link rel="stylesheet" href="./userSignUp_files/flag-icon.min.css">
+    <link rel="stylesheet" href="./userSignUp_files/vendor.bundle.base.css">
+    <!-- endinject -->
+    <!-- Plugin css for this page -->
+    <!-- End plugin css for this page -->
+    <!-- inject:css -->
+    <!-- endinject -->
+    <!-- Layout styles -->
+    <link rel="stylesheet" href="./userSignUp_files/style.css" <!--="" end="" layout="" styles="" --="">
+    <link rel="shortcut icon" href="https://www.bootstrapdash.com/demo/stellar-admin-free/jquery/images/favicon.png">
+  </head>
+  <body data-new-gr-c-s-check-loaded="14.1042.0" data-gr-ext-installed="">
+    <div class="container-scroller">
+      <div class="container-fluid page-body-wrapper full-page-wrapper">
+        <div class="content-wrapper d-flex align-items-center auth">
+          <div class="row flex-grow">
+            <div class="col-lg-4 mx-auto">
+              <div class="auth-form-light text-left p-5">
+                <div class="brand-logo">
+                  <img src="./userSignUp_files/logo.svg">
+                </div>
+                <h4>New User?</h4>
+                <h6 class="font-weight-light">Signing up is easy. It only takes a few steps</h6>
+                <form class="pt-3" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="POST">
+                  <div class="form-group">
+                    <input type="text" name="fullname" class="form-control form-control-lg" id="exampleInputUsername1" placeholder="Full Name" required>
+                  </div>
+                  <div class="form-group">
+                    <input type="text" name="username" class="form-control form-control-lg" id="exampleInputEmail1" placeholder="Username"  required>
+                  </div>
+                  <!-- <div class="form-group">
+                    <input type="text" class="form-control form-control-lg" id="exampleInputEmail1" placeholder="Email">
+                  </div> -->
+                  <!-- <div class="form-group">
+                    <select class="form-control form-control-lg" id="exampleFormControlSelect2">
+                      <option>Country</option>
+                      <option>United States of America</option>
+                      <option>United Kingdom</option>
+                      <option>India</option>
+                      <option>Germany</option>
+                      <option>Argentina</option>
+                    </select>
+                  </div> -->
+                  <div class="form-group">
+                    <input type="password" name="password" class="form-control form-control-lg <?php echo (!empty($password_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $password; ?>" id="exampleInputPassword1" placeholder="Password" required>
+                    <span class="invalid-feedback"><?php echo $password_err; ?></span>
+                  </div>
+                  <div class="form-group">
+                    <input type="password" name="confirm_password" class="form-control form-control-lg <?php echo (!empty($confirm_password_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $confirm_password; ?>" id="exampleInputPassword1" placeholder="Confirm Password" required>
+                    <span class="invalid-feedback"><?php echo $confirm_password_err; ?></span>
+                  </div>
+                  <div class="mb-4">
+                    <div class="form-check">
+                      <label class="form-check-label text-muted">
+                        <input type="checkbox" class="form-check-input" required> I agree to all Terms &amp; Conditions <i class="input-helper"></i></label>
+                    </div>
+                  </div>
+                  <div class="mt-3">
+                    <input type="submit" class="btn btn-block btn-primary btn-lg font-weight-medium auth-form-btn" value="Register">
+                  </div>
+                  <div class="text-center mt-4 font-weight-light"> Already have an account? <a href="login.html" class="text-primary">Login</a>
+                  </div>
+                </form>
               </div>
-              <h4>Add book</h4>
-              <!-- <h6 class="font-weight-light">Sign in to continue.</h6> -->
-              <form class="pt-12  " method="post" action="http://localhost/college/php-mini-project/user/login.php">
-                <div class="form-group">
-                  <input type="text" class="form-control form-control-lg" name="pid" id="exampleInputEmail1" placeholder="PID" required>
-                </div>
-                <div class="form-group">
-                   </div>
-                <div class="form-group">
-                  <input type="text" class="form-control form-control-lg" name="title" id="exampleInputEmail1" placeholder="Title" required>
-                </div>
-                <div class="form-group">
-                  <input type="text" class="form-control form-control-lg" name="author" id="exampleInputEmail1" placeholder="Author" required>
-                </div>
-
-                <div class="form-group">
-                  <input type="text" class="form-control form-control-lg" name="mrp" id="exampleInputEmail1" placeholder="MRP" required>
-                </div>
-                <div class="form-group">
-                  <input type="text" class="form-control form-control-lg" name="price" id="exampleInputEmail1" placeholder="Price" required>
-                </div>
-                <div class="form-group">
-                  <input type="text" class="form-control form-control-lg" name="discount" id="exampleInputEmail1" placeholder="Discount" required>
-                </div>
-                <div class="form-group">
-                  <input type="text" class="form-control form-control-lg" name="available" id="exampleInputEmail1" placeholder="Available" required>
-                </div><div class="form-group">
-                  <input type="text" class="form-control form-control-lg" name="publisher" id="exampleInputEmail1" placeholder="Publisher" required>
-                </div>
-                <div class="form-group">
-                  <input type="text" class="form-control form-control-lg" name="edition" id="exampleInputEmail1" placeholder="Edition" required>
-                </div>
-
-                <div class="form-group">
-                  <input type="text" class="form-control form-control-lg" name="category" id="exampleInputEmail1" placeholder="Category" required>
-                </div><div class="form-group">
-                  <input type="text" class="form-control form-control-lg" name="description" id="exampleInputEmail1" placeholder="Description" required>
-                </div>
-                <div class="form-group">
-                  <input type="text" class="form-control form-control-lg" name="language" id="exampleInputEmail1" placeholder="Language" required>
-                </div>
-                <div class="form-group">
-                  <input type="text" class="form-control form-control-lg" name="page" id="exampleInputEmail1" placeholder="Page" required>
-                </div>
-                <div class="form-group">
-                  <input type="text" class="form-control form-control-lg" name="weight" id="exampleInputEmail1" placeholder="Weight" required>
-                </div>
-                <div class="mt-3">
-                  <input type="submit" name="storebook" class="btn btn-block btn-primary btn-lg font-weight-medium auth-form-btn" value="Store Book">
-                </div>
-               </div>
-
-              </form>
             </div>
           </div>
         </div>
+        <!-- content-wrapper ends -->
       </div>
-      <!-- content-wrapper ends -->
+      <!-- page-body-wrapper ends -->
     </div>
-    <!-- page-body-wrapper ends -->
-  </div>
-  <!-- container-scroller -->
-  <!-- plugins:js -->
-  <script src="./newUser_files/vendor.bundle.base.js.download"></script>
-  <!-- endinject -->
-  <!-- Plugin js for this page -->
-  <!-- End plugin js for this page -->
-  <!-- inject:js -->
-  <script src="./newUser_files/off-canvas.js.download"></script>
-  <script src="./newUser_files/misc.js.download"></script>
-  <!-- endinject -->
-
-  <div id="squirrelson" style="position: fixed; bottom: -5%; left: -100%; z-index: 9999; visibility: hidden;"><img src="chrome-extension://mdafemlphdjnhbdglfnankciemdngach/squirrels/squirrel_1.png" width="350px" height="271px" id="squirrelImage"></div>
-
-<grammarly-desktop-integration data-grammarly-shadow-root="true"></grammarly-desktop-integration>
-
-<div id="squirrelson" style="position: fixed; bottom: -5%; left: -100%; z-index: 9999; visibility: hidden;"><img src="chrome-extension://mdafemlphdjnhbdglfnankciemdngach/squirrels/squirrel_1.png" width="350px" height="271px" id="squirrelImage"></div></body></html>
+    <!-- container-scroller -->
+    <!-- plugins:js -->
+    <script src="./userSignUp_files/vendor.bundle.base.js.download"></script>
+    <!-- endinject -->
+    <!-- Plugin js for this page -->
+    <!-- End plugin js for this page -->
+    <!-- inject:js -->
+    <script src="./userSignUp_files/off-canvas.js.download"></script>
+    <script src="./userSignUp_files/misc.js.download"></script>
+    <!-- endinject -->
+  
+<div id="squirrelson" style="position: fixed; bottom: -5%; left: -100%; z-index: 9999; visibility: hidden;"><img src="chrome-extension://mdafemlphdjnhbdglfnankciemdngach/squirrels/squirrel_1.png" width="350px" height="271px" id="squirrelImage"></div></body><grammarly-desktop-integration data-grammarly-shadow-root="true"></grammarly-desktop-integration></html>
